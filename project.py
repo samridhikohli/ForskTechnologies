76bb20cbc4c6b86a916fbc817f6f202c7a9f2519
Samridhi Kohli <42908015+samridhikohli@users.noreply.github.com>
2021-11-13 16:39:21 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 24 17:19:02 2018
+
+@author: MUJ
+"""
+
+#DIMENSION REDUCTION
+
+#want to visualize the data
+#coverting the 2D data to 1D
+#by using PRINCIPLE COMPONENT ANALYSIS|PCA
+#there will be some information loss
+
+#BACKWARD ELIMINATION under supervised
+#PCA under unsupervised 
+
+
+
